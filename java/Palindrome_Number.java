8eb56acb6fab7a81d8e1dd0dec8410df1c49cc16
pit <notthewitcher@gmail.com>
2026-04-05 00:15:28 +0530

    added palindrome number

---
+++ java/Palindrome_Number.java
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/palindrome-number/
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        // negative numbers are not palindrome
+        if (x < 0) return false;
+
+        int original = x;
+        int reversed = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+
+        return original == reversed;
+    }
+}
