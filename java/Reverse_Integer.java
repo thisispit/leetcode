f04694625406c4892d5de4a64ba08140c54f1e72
pit <notthewitcher@gmail.com>
2026-05-25 01:45:30 +0530

    Add Reverse Integer solution

---
+++ java/Reverse_Integer.java
@@ -0,0 +1,14 @@
+// Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-2^31, 2^31 - 1], then return 0.
+class Solution {
+    public int reverse(int x) {
+        long rev = 0;
+        while (x != 0) {
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+        if (rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE) {
+            return 0;
+        }
+        return (int) rev;
+    }
+}
