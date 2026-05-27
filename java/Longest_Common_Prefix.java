0ad12892af0d41a57a13b2b1a76d69b4827cb4af
pit <notthewitcher@gmail.com>
2026-05-27 09:40:49 +0530

    Add Longest Common Prefix solution

---
+++ java/Longest_Common_Prefix.java
@@ -0,0 +1,19 @@
+public class Longest_Common_Prefix {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        }
+        return prefix;
+    }
+
+    public static void main(String[] args) {
+        Longest_Common_Prefix lcp = new Longest_Common_Prefix();
+        String[] strs = {"flower", "flow", "flight"};
+        System.out.println("Longest Common Prefix: " + lcp.longestCommonPrefix(strs));
+    }
+}
