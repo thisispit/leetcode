a9a9d015d6432c5d287cd65fbcbc7a4ed8e131dc
pit <notthewitcher@gmail.com>
2026-05-30 15:04:41 +0530

    Add Valid Anagram solution

---
+++ java/Valid_Anagram.java
@@ -0,0 +1,19 @@
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] count = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+        for (int c : count) {
+            if (c != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
