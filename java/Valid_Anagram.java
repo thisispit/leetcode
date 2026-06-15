6bf39835ca91572feb53e9cab946d0e2e4b1df07
pit <notthewitcher@gmail.com>
2026-06-15 14:44:10 +0530

    optimize 242. Valid Anagram - use frequency array O(n)

--- java/Valid_Anagram.java
+++ java/Valid_Anagram.java
@@ -1,18 +1,14 @@
+// LeetCode 242 - Valid Anagram
 // Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+import java.util.Arrays;
+
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
-        }
+        if (s.length() != t.length()) return false;
         int[] count = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            count[s.charAt(i) - 'a']++;
-            count[t.charAt(i) - 'a']--;
-        }
-        for (int c : count) {
-            if (c != 0) {
-                return false;
-            }
+        for (char c : s.toCharArray()) count[c - 'a']++;
+        for (char c : t.toCharArray()) {
+            if (--count[c - 'a'] < 0) return false;
         }
         return true;
     }
