2ae9e3c920e50d2ca62560ffdd1a32c2449f4adf
pit <notthewitcher@gmail.com>
2026-06-29 10:44:35 +0530

    Refactor: Find First and Last Position using unified binarySearch helper

--- java/Find_First_and_last_Position.java
+++ java/Find_First_and_last_Position.java
@@ -1,17 +1,17 @@
-// Find First and Last Position of Element in Sorted Array 
+// Find First and Last Position of Element in Sorted Array
 // Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+// If target is not found in the array, return [-1, -1].
+// You must write an algorithm with O(log n) runtime complexity.
 
-If target is not found in the array, return [-1, -1].
-
-You must write an algorithm with O(log n) runtime complexity.
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int first = findFirst(nums, target);
-        int last = findLast(nums, target);
-        return new int[]{first, last};
+        return new int[]{
+            binarySearch(nums, target, true),
+            binarySearch(nums, target, false)
+        };
     }
 
-    private int findFirst(int[] nums, int target) {
+    public int binarySearch(int[] nums, int target, boolean first) {
         int left = 0, right = nums.length - 1;
         int ans = -1;
 
@@ -20,7 +20,10 @@ class Solution {
 
             if (nums[mid] == target) {
                 ans = mid;
-                right = mid - 1;
+                if (first)
+                    right = mid - 1;
+                else
+                    left = mid + 1;
             } 
             else if (nums[mid] < target) {
                 left = mid + 1;
@@ -29,27 +32,7 @@ class Solution {
                 right = mid - 1;
             }
         }
-        return ans;
-    }
 
-    private int findLast(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-        int ans = -1;
-
-        while (left <= right) { 
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] == target) {
-                ans = mid;
-                left = mid + 1; // search right side
-            } 
-            else if (nums[mid] < target) {
-                left = mid + 1;
-            } 
-            else {
-                right = mid - 1;
-            }
-        }
         return ans;
     }
 }
